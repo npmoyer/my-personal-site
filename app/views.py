e896740dd7d19d96b9303d4a5a08ef36508d4d5f
Nick Moyer <moyer.n.p@gmail.com>
2022-11-05 23:48:02 -0500

    set view and template

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.views.generic.base import TemplateView
+
+class Index(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
